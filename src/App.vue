ff1dae8b1c162371097b19450e277f5ed8efda00
fifthThirteen <570205219@qq.com>
2023-11-03 22:14:21 +0800

    fix:upload slot

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div style="max-width: 980px; margin: 100px auto">
-    <Upload @success="handleSuccess" />
-    <Facedat v-if="isFace" :data="this.data" :lock="true" />
-    <Bodydat v-if="isBody" :data="this.data" :lock="false" />
-  </div>
+    <div style="max-width: 980px; margin: 100px auto">
+        <Upload @success="handleSuccess" />
+        <Facedat v-if="isFace" :data="this.data" :lock="true" />
+        <Bodydat v-if="isBody" :data="this.data" :lock="false" />
+    </div>
 </template>
 
 <script>
@@ -11,31 +11,31 @@ import Facedat from "./Facedat";
 import Bodydat from "./Bodydat";
 import Upload from "./Upload";
 export default {
-  props: [],
-  data: function () {
-    return {
-      data: "",
-      value: 0
-    };
-  },
-  computed: {
-    isFace: function () {
-      return this.data.type?.startsWith("face");
+    props: [],
+    data: function () {
+        return {
+            data: "",
+            value: 0,
+        };
     },
-    isBody: function () {
-      return this.data.type === "body";
+    computed: {
+        isFace: function () {
+            return this.data.type?.startsWith("face");
+        },
+        isBody: function () {
+            return this.data.type === "body";
+        },
     },
-  },
-  methods: {
-    handleSuccess: function (data) {
-      this.data = data;
+    methods: {
+        handleSuccess: function (data) {
+            this.data = data;
+        },
     },
-  },
-  mounted: function () {},
-  components: {
-    Facedat,
-    Bodydat,
-    Upload,
-  },
+    mounted: function () {},
+    components: {
+        Facedat,
+        Bodydat,
+        Upload,
+    },
 };
 </script>
